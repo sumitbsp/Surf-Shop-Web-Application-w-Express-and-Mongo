4d1825cc88c118b2bfca014d4de5ca3a668ea2a2
sumitbsp <sumit.bsp88@gmail.com>
2020-03-05 01:24:15 +0530

    Add user and index routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const reviewsRouter = require('./routes/reviews');
 const postsRouter = require('./routes/posts');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 app.use('/posts', postsRouter);
 
